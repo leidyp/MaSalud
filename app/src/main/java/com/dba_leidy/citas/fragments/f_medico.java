6528ea9c6c6d9e6b75a3aec2a2e2538c96d130b4
Leidy Acuna <leidyacuna03@gmail.com>
2017-11-23 14:25:35 -0500

    interfaz medico

--- app/src/main/java/com/dba_leidy/citas/fragments/f_medico.java
+++ app/src/main/java/com/dba_leidy/citas/fragments/f_medico.java
@@ -6,14 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.codetroopers.betterpickers.calendardatepicker.MonthAdapter.CalendarDay;
+import com.dba_leidy.citas.Crud;
 import com.dba_leidy.citas.R;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+import java.util.Calendar;
 
 /**
- * Created by DBA-Leidy on 10/11/2017.
+ * Created by DBA-Leidy on 22/11/2017.
  */
 
-public class f_medico extends Fragment {
+public class f_medico extends Fragment implements CalendarDatePickerDialogFragment.OnDateSetListener {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private Button button;
+    private int year, month, day;
+    Crud c;
+
+    String[] SPINNERLIST = {"User1", "User2", "User3"};
+
+    MaterialBetterSpinner usuar;
+    int positionP = -1;
 
     @Nullable
     @Override
@@ -27,7 +50,51 @@ public class f_medico extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
+        usuar = (MaterialBetterSpinner) getView().findViewById(R.id.usuariom);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
+
+        dateView = (TextView) getView().findViewById(R.id.fechanm);
+        c = new Crud(getContext());
+
+        usuar.setAdapter(arrayAdapter);
+        usuar.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                positionP = position;
+            }
+        });
         getActivity().setTitle("Registro Medicos");
+
+        dateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                int year = Calendar.getInstance().get(Calendar.YEAR);
+                int month = Calendar.getInstance().get(Calendar.MONTH);
+                int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+                CalendarDay date = new CalendarDay();
+                CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
+                        .setOnDateSetListener(f_medico.this)
+                        .setFirstDayOfWeek(Calendar.SUNDAY)
+                        .setPreselectedDate(year,month, day)
+                        .setDateRange(null,date)
+                        .setDoneText("OK")
+                        .setCancelText("Cancelar");
+                cdp.show(getActivity().getSupportFragmentManager(),"Fecha de nacimiento");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
+        monthOfYear += 1;
+        dayOfMonth += 1;
+        String mes = "" + (monthOfYear < 10 ? "0" + monthOfYear : monthOfYear);
+        String dia = "" + (dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth);
+        dateView.setText(year + "-" + mes + "-" + dia);
+    }
+
+    public void insertPatient() {
+
     }
 }
--- app/src/main/java/com/dba_leidy/citas/fragments/f_recepcionista.java
+++ app/src/main/java/com/dba_leidy/citas/fragments/f_recepcionista.java
@@ -8,36 +8,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.codetroopers.betterpickers.calendardatepicker.MonthAdapter.CalendarDay;
+import com.dba_leidy.citas.Crud;
 import com.dba_leidy.citas.R;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+import java.util.Calendar;
 
 /**
  * Created by DBA-Leidy on 22/11/2017.
  */
 
-public class f_recepcionista extends Fragment {
+public class f_recepcionista extends Fragment implements CalendarDatePickerDialogFragment.OnDateSetListener {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private Button button;
+    private int year, month, day;
+    Crud c;
 
     String[] SPINNERLIST = {"User1", "User2", "User3"};
 
     MaterialBetterSpinner usuar;
     int positionP = -1;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_recepcionista , container, false);
+        return inflater.inflate(R.layout.fragment_recepcionista, container, false);
     }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
-        getActivity().setTitle("Registro Recepcionistas");
-        usuar = (MaterialBetterSpinner)getView().findViewById(R.id.usuar);
+        usuar = (MaterialBetterSpinner) getView().findViewById(R.id.usuar);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
+
+        dateView = (TextView) getView().findViewById(R.id.fechanr);
+        c = new Crud(getContext());
 
         usuar.setAdapter(arrayAdapter);
         usuar.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -46,5 +63,38 @@ public class f_recepcionista extends Fragment {
                 positionP = position;
             }
         });
+        getActivity().setTitle("Registro Recepcionistas");
+
+        dateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                int year = Calendar.getInstance().get(Calendar.YEAR);
+                int month = Calendar.getInstance().get(Calendar.MONTH);
+                int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+                CalendarDay date = new CalendarDay();
+                CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
+                        .setOnDateSetListener(f_recepcionista.this)
+                        .setFirstDayOfWeek(Calendar.SUNDAY)
+                        .setPreselectedDate(year,month, day)
+                        .setDateRange(null,date)
+                        .setDoneText("OK")
+                        .setCancelText("Cancelar");
+                cdp.show(getActivity().getSupportFragmentManager(),"Fecha de nacimiento");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
+        monthOfYear += 1;
+        dayOfMonth += 1;
+        String mes = "" + (monthOfYear < 10 ? "0" + monthOfYear : monthOfYear);
+        String dia = "" + (dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth);
+        dateView.setText(year + "-" + mes + "-" + dia);
+    }
+
+    public void insertPatient() {
+
     }
 }
